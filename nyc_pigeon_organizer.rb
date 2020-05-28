dca75308dee70143a9a0ab74a7bab1070b416aba
Zana H <zhertica@gmail.com>
2020-05-28 04:19:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,20 @@
 def nyc_pigeon_organizer(data)
   final_array={}
-  data.each_with_object({}) do |key, value, final_array|
+  data.each_with_object({}) do |key, value|
     value.each do |inner_key, names|
       names.each do |name|
+        
         if !final_array[name]
           final_array[name] = {}
         end
+        
         if !final_array[name][key]
           final_array[name][key] = []
         end
+        
         final_array[name][key].push(inner_key.to_s)
       end
     end
   end
+  final_array
 end
