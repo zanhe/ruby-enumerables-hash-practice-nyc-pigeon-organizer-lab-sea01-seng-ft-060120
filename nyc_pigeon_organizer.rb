2289350d18f6403500aea0e9f11f3f2cb85f55cb
Zana H <zhertica@gmail.com>
2020-05-28 04:21:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_array = {}
+  data.each do |key, value|
+    value.each do |inner_key, names|
+      names.each do |name|
+        
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        
+        if !final_array[name][key]
+          final_array[name][key] = []
+        end
+        
+        final_array[name][key].push(inner_key.to_s)
+      end
+    end
+  end
+  final_array
 end
